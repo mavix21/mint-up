6fd75380f2792b24a9ac582dc83553fdbd79c369
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-11 17:16:42 -0500

    feat: integrate wallet functionality with OnchainKit in mini app

--- apps/mini/app/page.tsx
+++ apps/mini/app/page.tsx
@@ -1,6 +1,13 @@
 'use client';
 
+import { Name, Identity, Address, Avatar, EthBalance } from '@coinbase/onchainkit/identity';
 import { useMiniKit } from '@coinbase/onchainkit/minikit';
+import {
+  ConnectWallet,
+  Wallet,
+  WalletDropdown,
+  WalletDropdownDisconnect,
+} from '@coinbase/onchainkit/wallet';
 import { Button, YStack } from '@my/ui';
 import { useEffect } from 'react';
 
@@ -25,6 +32,20 @@ export default function HomePage() {
     >
       <YStack bg="$background">
         <Button>Click me</Button>
+        <Wallet className="z-10">
+          <ConnectWallet>
+            <Name className="text-inherit" />
+          </ConnectWallet>
+          <WalletDropdown>
+            <Identity className="px-4 pt-3 pb-2" hasCopyAddressOnClick>
+              <Avatar />
+              <Name />
+              <Address />
+              <EthBalance />
+            </Identity>
+            <WalletDropdownDisconnect />
+          </WalletDropdown>
+        </Wallet>
       </YStack>
     </YStack>
   );
