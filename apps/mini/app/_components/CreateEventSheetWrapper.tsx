f0ac66492a95e56e99de85341f9e42354359ed98
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-12 12:01:35 -0500

    feat: add CreateEventSheetWrapper component for event creation with customizable snap points and overlay

---
+++ apps/mini/app/_components/CreateEventSheetWrapper.tsx
@@ -0,0 +1,47 @@
+import { Sheet } from '@my/ui';
+import { CreateEventScreen } from 'app/screens/create-event/create-event-screen';
+import { memo, useState } from 'react';
+
+interface CreateEventSheetWrapperProps {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}
+
+export function CreateEventSheetWrapper({ open, setOpen }: CreateEventSheetWrapperProps) {
+  const [position, setPosition] = useState(0);
+  const snapPoints = [95, 50, 25];
+
+  return (
+    <Sheet
+      dismissOnSnapToBottom
+      modal
+      open={open}
+      onOpenChange={setOpen}
+      position={position}
+      onPositionChange={setPosition}
+      zIndex={100_000}
+      snapPoints={snapPoints}
+      snapPointsMode="percent"
+      animation="medium"
+    >
+      <Sheet.Overlay
+        animation="lazy"
+        backgroundColor="$shadowColor"
+        enterStyle={{ opacity: 0 }}
+        exitStyle={{ opacity: 0 }}
+      />
+      <Sheet.Handle backgroundColor="$color2" />
+      <Sheet.Frame alignItems="center" bg="$color2" pt="$4">
+        <SheetContents />
+      </Sheet.Frame>
+    </Sheet>
+  );
+}
+
+const SheetContents = memo(() => {
+  return (
+    <>
+      <CreateEventScreen />
+    </>
+  );
+});
