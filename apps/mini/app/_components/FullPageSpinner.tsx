f2756b2afc5ae14c082e46d8d024ca8758994cdd
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-13 16:59:09 -0500

    feat: add FullPageSpinner component for improved loading indication

---
+++ apps/mini/app/_components/FullPageSpinner.tsx
@@ -0,0 +1,11 @@
+import { Spinner, Theme, YStack } from '@my/ui';
+
+export function FullPageSpinner() {
+  return (
+    <YStack flex={1} justifyContent="center" alignItems="center" height={'100vh' as any}>
+      <Theme name="green">
+        <Spinner size="large" color="$color10" />
+      </Theme>
+    </YStack>
+  );
+}
