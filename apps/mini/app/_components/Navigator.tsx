a698247d383b0b3ac949fd2b039fde1e974f555e
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-13 16:59:18 -0500

    feat: create Navigator component to manage tab navigation and event creation

---
+++ apps/mini/app/_components/Navigator.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { MyEventsScreen } from 'app/screens/my-events/my-events-screen';
+import { memo, useState } from 'react';
+
+import { BottomTabNav } from './BottomTabNav';
+import { CreateEventSheetWrapper } from './CreateEventSheetWrapper';
+import { TabSelector } from './TabSelector';
+
+export const Navigator = memo(function Navigator() {
+  const [activeTab, setActiveTab] = useState('my-events');
+  const [open, setOpen] = useState(false);
+  console.log('Navigator');
+
+  return (
+    <>
+      <BottomTabNav
+        activeTab={activeTab}
+        setActiveTab={(tab) => {
+          if (tab === 'create') {
+            setOpen(true);
+          } else {
+            setActiveTab(tab);
+          }
+        }}
+      />
+      <TabSelector activeTab={activeTab} />
+      <CreateEventSheetWrapper open={open} setOpen={setOpen} />
+    </>
+  );
+});
