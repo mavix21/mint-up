a30775b4b013cb6d0745b73132fdbb9bd191e04f
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-13 16:58:53 -0500

    refactor: simplify HomePage component by integrating App and adding loading spinner

--- apps/mini/app/(app)/page.tsx
+++ apps/mini/app/(app)/page.tsx
@@ -1,48 +1,25 @@
 'use client';
 
 import { useMiniKit } from '@coinbase/onchainkit/minikit';
-import { Button, XStack, YStack } from '@my/ui';
-import { MyEventsScreen } from 'app/screens/my-events/my-events-screen';
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 
-import { BottomTabNav } from '../_components/BottomTabNav';
-import { CreateEventSheetWrapper } from '../_components/CreateEventSheetWrapper';
+import App from './app';
+import { FullPageSpinner } from '../_components/FullPageSpinner';
 
 export default function HomePage() {
   const { setFrameReady, isFrameReady } = useMiniKit();
-  const [open, setOpen] = useState(false);
-  const [activeTab, setActiveTab] = useState('my-events');
+
   // The setFrameReady() function is called when your mini-app is ready to be shown
   useEffect(() => {
     if (!isFrameReady) {
-      setFrameReady();
+      setFrameReady().then(() => console.log('Mini app loaded'));
     }
   }, [setFrameReady, isFrameReady]);
+  console.log('HomePage');
 
-  return (
-    <>
-      <YStack
-        flex={1}
-        justifyContent="center"
-        alignItems="center"
-        height={'100vh' as any}
-        position="relative"
-        gap={20}
-        bg="$background"
-      >
-        <BottomTabNav
-          activeTab={activeTab}
-          setActiveTab={(tab) => {
-            if (tab === 'create') {
-              setOpen(true);
-            } else {
-              setActiveTab(tab);
-            }
-          }}
-        />
-        {activeTab === 'my-events' && <MyEventsScreen />}
-      </YStack>
-      <CreateEventSheetWrapper open={open} setOpen={setOpen} />
-    </>
-  );
+  if (!isFrameReady) {
+    return <FullPageSpinner />;
+  }
+
+  return <App />;
 }
