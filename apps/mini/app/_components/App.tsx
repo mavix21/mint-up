c4fe4f81857684dfcad2c1b6517d69ede32dfc24
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-13 21:49:22 -0500

    refactor: remove gap property from App component for cleaner layout

--- apps/mini/app/_components/App.tsx
+++ apps/mini/app/_components/App.tsx
@@ -11,7 +11,6 @@ export default function App() {
       alignItems="center"
       position="relative"
       minHeight={'100vh' as any}
-      gap={20}
       bg="$background"
     >
       <Navigator />
