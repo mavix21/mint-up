aa8f997f4e7259ebba8ef7ddf5df7046daad2a40
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-19 20:24:24 -0500

    fix: update FullPageSpinner height to use '100svh' for better responsiveness

--- apps/mini/app/_components/FullPageSpinner.tsx
+++ apps/mini/app/_components/FullPageSpinner.tsx
@@ -2,7 +2,7 @@ import { Spinner, Theme, YStack } from '@my/ui';
 
 export function FullPageSpinner() {
   return (
-    <YStack flex={1} justifyContent="center" alignItems="center" height={'100vh' as any}>
+    <YStack flex={1} justifyContent="center" alignItems="center" height={'100svh' as any}>
       <Theme name="green">
         <Spinner size="large" color="$color10" />
       </Theme>
