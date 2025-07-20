f9f71ed6c441a4324e4cccabd99611cb5a255553
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-19 21:00:14 -0500

    feat: update metadata generation to utilize environment variables for dynamic content

--- apps/mini/app/(app)/layout.tsx
+++ apps/mini/app/(app)/layout.tsx
@@ -5,10 +5,29 @@ import { Providers } from '@/providers';
 import '@coinbase/onchainkit/styles.css';
 import './app.css';
 
-export const metadata: Metadata = {
-  title: 'Mini',
-  description: 'Mini',
-};
+export async function generateMetadata(): Promise<Metadata> {
+  const URL = process.env.NEXT_PUBLIC_URL;
+  return {
+    title: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+    description: process.env.NEXT_PUBLIC_APP_DESCRIPTION,
+    other: {
+      'fc:frame': JSON.stringify({
+        version: 'next',
+        imageUrl: process.env.NEXT_PUBLIC_APP_HERO_IMAGE,
+        button: {
+          title: `Launch ${process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME}`,
+          action: {
+            type: 'launch_frame',
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME,
+            url: URL,
+            splashImageUrl: process.env.NEXT_PUBLIC_APP_SPLASH_IMAGE,
+            splashBackgroundColor: process.env.NEXT_PUBLIC_SPLASH_BACKGROUND_COLOR,
+          },
+        },
+      }),
+    },
+  };
+}
 
 export default async function RootLayout({
   children,
