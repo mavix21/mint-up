83e5d2dffffcd67df19919911450534655e85292
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-23 15:16:58 -0500

    fix: prevent unnecesary rerenders of the create event screen

--- apps/mini/app/_components/CreateEventSheetWrapper.tsx
+++ apps/mini/app/_components/CreateEventSheetWrapper.tsx
@@ -34,30 +34,28 @@ export function CreateEventSheetWrapper({ open, setOpen }: CreateEventSheetWrapp
         alignItems="center"
         style={{ height: visualViewportHeight }}
       >
-        <SheetContents onClose={() => setOpen(false)} />
+        <YStack flex={1} width="100%">
+          {/* Close Button */}
+          <XStack
+            position="absolute"
+            top="$2"
+            right="$2"
+            zIndex={1000}
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Button size="$2" circular borderWidth={1} onPress={() => setOpen(false)}>
+              ✕
+            </Button>
+          </XStack>
+
+          <SheetContents />
+        </YStack>
       </Sheet.Frame>
     </Sheet>
   );
 }
 
-const SheetContents = memo(({ onClose }: { onClose: () => void }) => {
-  return (
-    <YStack flex={1} width="100%">
-      {/* Close Button */}
-      <XStack
-        position="absolute"
-        top="$2"
-        right="$2"
-        zIndex={1000}
-        alignItems="center"
-        justifyContent="center"
-      >
-        <Button size="$2" circular borderWidth={1} onPress={onClose}>
-          ✕
-        </Button>
-      </XStack>
-
-      <CreateEventScreen />
-    </YStack>
-  );
+const SheetContents = memo(() => {
+  return <CreateEventScreen />;
 });
