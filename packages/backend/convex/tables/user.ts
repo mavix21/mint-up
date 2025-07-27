a14a734346c29dbec708451ab7c605024825d4a2
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-27 18:24:23 -0500

    feat: update usersTable to make username and pfpUrl required fields, and rename primaryWalletAddress to currentWalletAddress

--- packages/backend/convex/tables/user.ts
+++ packages/backend/convex/tables/user.ts
@@ -2,13 +2,13 @@ import { defineTable } from 'convex/server';
 import { v } from 'convex/values';
 
 export const usersTable = defineTable({
+  username: v.string(),
+  pfpUrl: v.string(),
   displayName: v.optional(v.string()),
-  username: v.optional(v.string()),
-  pfpUrl: v.optional(v.string()),
   bio: v.optional(v.string()),
   email: v.optional(v.string()),
   emailVerified: v.optional(v.number()),
-  primaryWalletAddress: v.optional(v.string()),
+  currentWalletAddress: v.optional(v.string()),
 })
-  .index('by_primaryWalletAddress', ['primaryWalletAddress'])
+  .index('by_currentWalletAddress', ['currentWalletAddress'])
   .index('by_email', ['email']);
