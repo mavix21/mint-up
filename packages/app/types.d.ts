b5f7599f4a0cfa5d28c4547e336b0de73024f3a3
mavix21 <marcevizcarra2109@hotmail.com>
2025-07-29 18:13:37 -0500

    feat: extend next-auth Session type to include convexToken and user details

--- packages/app/types.d.ts
+++ packages/app/types.d.ts
@@ -5,3 +5,16 @@ export type Conf = typeof config;
 declare module '@my/ui' {
   interface TamaguiCustomConfig extends Conf {}
 }
+
+declare module 'next-auth' {
+  interface Session {
+    convexToken: string;
+    user: {
+      id: Id<'users'>;
+      fid: number;
+      username: string;
+      image: string;
+      currentWalletAddress: `0x${string}`;
+    };
+  }
+}
