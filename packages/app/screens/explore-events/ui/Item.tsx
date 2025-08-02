46bf377ae9d7461fecb8eb29ccc6ecb3c31e7217
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2025-08-01 21:29:27 -0500

    fix: add background color to Avatar.Image in Item component for better visual consistency

--- packages/app/screens/explore-events/ui/Item.tsx
+++ packages/app/screens/explore-events/ui/Item.tsx
@@ -4,7 +4,7 @@ import type { SizeTokens } from '@my/ui';
 export function Item({ imageUrl, size }: { imageUrl: string; size: SizeTokens }) {
   return (
     <Avatar size={size} circular>
-      <Avatar.Image objectFit="cover" src={imageUrl} />
+      <Avatar.Image objectFit="cover" src={imageUrl} backgroundColor="$background" />
       <Avatar.Fallback backgroundColor="$background" />
     </Avatar>
   );
