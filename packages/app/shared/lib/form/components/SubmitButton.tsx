362107fafc554c1cb773b06c9103d8897225ca15
mavix21 <marcevizcarra2109@hotmail.com>
2025-08-02 18:49:16 -0500

    refactor: update SubmitButton to improve loading state handling and enhance button structure

--- packages/app/shared/lib/form/components/SubmitButton.tsx
+++ packages/app/shared/lib/form/components/SubmitButton.tsx
@@ -1,43 +1,42 @@
-// /**
-//  * created to be used in forms
-//  * will show loading spinners and disable submission when already submitting
-
-import { AnimatePresence, Button, ButtonProps, Spinner } from '@my/ui';
+import { Button, ButtonProps, Spinner, View } from '@my/ui';
 
 import { useFormContext } from '../context';
 
+// /**
+//  * created to be used in forms
+//  * will show loading spinners and disable submission when already submitting
 //  */
-export const SubmitButton = (props: ButtonProps) => {
+export const SubmitButton = (props: ButtonProps & { label: string }) => {
   const form = useFormContext();
 
   return (
     <form.Subscribe selector={(state) => state.isSubmitting}>
       {(isSubmitting) => (
-        <Button
-          iconAfter={
-            <AnimatePresence>
-              {isSubmitting && (
-                <Spinner
-                  color="$color"
-                  key="loading-spinner"
-                  o={1}
-                  y={0}
-                  animation="quick"
-                  enterStyle={{
-                    o: 0,
-                    y: 4,
-                  }}
-                  exitStyle={{
-                    o: 0,
-                    y: 4,
-                  }}
-                />
-              )}
-            </AnimatePresence>
-          }
-          disabled={isSubmitting}
-          {...props}
-        />
+        <Button disabled={isSubmitting} {...props}>
+          <View
+            animation="bouncy"
+            flexDirection="row"
+            x={isSubmitting ? 0 : 15}
+            gap="$3"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Button.Text>{props.label}</Button.Text>
+            <Button.Icon>
+              <Spinner
+                key="loading-spinner"
+                animation="slow"
+                enterStyle={{
+                  scale: 1,
+                }}
+                exitStyle={{
+                  scale: 1,
+                }}
+                opacity={isSubmitting ? 1 : 0}
+              />
+            </Button.Icon>
+          </View>
+        </Button>
       )}
     </form.Subscribe>
   );
