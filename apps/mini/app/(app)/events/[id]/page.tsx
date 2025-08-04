ccd630dfd12d7439e90d3f17fbd26c538bda1934
Gianella Xiomara Coronel Manchego <gia.ne12@hotmail.com>
2025-08-03 20:05:08 -0500

    update cost to function

--- apps/mini/app/(app)/events/[id]/page.tsx
+++ apps/mini/app/(app)/events/[id]/page.tsx
@@ -3,8 +3,8 @@
 import { Text } from '@my/ui';
 import { useParams } from 'solito/navigation';
 
-export const EventDescriptionScreen = () => {
+export default function EventDescriptionScreen() {
   const { id } = useParams<{ id: string }>();
 
   return <Text>Event: {id}</Text>;
-};
+}
