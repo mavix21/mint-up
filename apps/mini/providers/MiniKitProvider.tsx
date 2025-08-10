d830da6d8b2abd3ca609e35bc3154406706cb456
mavix21 <marcevizcarra2109@hotmail.com>
2025-08-10 16:57:56 -0500

    feat: update MiniKitProvider to use environment-based chain selection for improved flexibility

--- apps/mini/providers/MiniKitProvider.tsx
+++ apps/mini/providers/MiniKitProvider.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { MiniKitProvider } from '@coinbase/onchainkit/minikit';
+import { isDevelopment } from 'app/shared/lib/environment';
 import { ReactNode } from 'react';
-import { base } from 'wagmi/chains';
+import { base, baseSepolia } from 'viem/chains';
 
 export function MiniKitContextProvider({ children }: { children: ReactNode }) {
+  const chain = isDevelopment() ? baseSepolia : base;
   return (
-    <MiniKitProvider apiKey={process.env.NEXT_PUBLIC_CDP_CLIENT_API_KEY} chain={base}>
+    <MiniKitProvider apiKey={process.env.NEXT_PUBLIC_CDP_CLIENT_API_KEY} chain={chain}>
       {children}
     </MiniKitProvider>
   );
