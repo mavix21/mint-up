23d47dd86e4e3a8eaa1b86f8a35208f9629b4c0e
mavix21 <marcevizcarra2109@hotmail.com>
2025-08-16 12:38:21 -0500

    fix(Navigator): update overflow property to overflowBlock hidden for consistent scrolling behavior

--- apps/mini/app/_components/Navigator.tsx
+++ apps/mini/app/_components/Navigator.tsx
@@ -40,7 +40,7 @@ export const Navigator = memo(function Navigator() {
     <>
       <YStack width="100%" height="100%" position="relative" overflowBlock="hidden">
         <Topbar />
-        <YStack flex={1} width="100%" height="100%" overflow="hidden">
+        <YStack flex={1} width="100%" height="100%" overflowBlock="hidden">
           <TabSelector activeTab={activeTab} />
         </YStack>
         <BottomTabNav activeTab={activeTab} setActiveTab={handleTabChange} />
