739cf3d497786863f4314d7443f6e084d3d04685
mavix21 <marcevizcarra2109@hotmail.com>
2025-08-17 13:16:02 -0500

    style(app.css, TestPage.tsx): update body height to '100dvh' and add overflow properties for improved layout and scrolling behavior

--- apps/mini/app/_pages/TestPage.tsx
+++ apps/mini/app/_pages/TestPage.tsx
@@ -2,7 +2,7 @@ import { Text, View } from '@my/ui';
 
 export function TestPage() {
   return (
-    <View>
+    <View overflow="hidden" style={{ overscrollBehavior: 'contain' }}>
       <Text>TestPage</Text>
     </View>
   );
