173d5bcda84db95e39b097d9b203b331188a88a4
mavix21 <marcevizcarra2109@hotmail.com>
2025-08-17 17:59:29 -0500

    feat(FullscreenSpinner): enhance spinner component with theme support and full viewport height

--- packages/ui/src/components/FullscreenSpinner.tsx
+++ packages/ui/src/components/FullscreenSpinner.tsx
@@ -1,9 +1,11 @@
-import { Spinner, SpinnerProps, YStack } from 'tamagui';
+import { Spinner, SpinnerProps, Theme, YStack } from 'tamagui';
 
 export const FullscreenSpinner = (props: SpinnerProps) => {
   return (
-    <YStack f={1} jc="center" ai="center">
-      <Spinner {...props} />
+    <YStack f={1} jc="center" ai="center" height={'100vh' as any}>
+      <Theme name={props.theme ?? 'green'}>
+        <Spinner color="$color10" size={props.size ?? 'large'} {...props} />
+      </Theme>
     </YStack>
   );
 };
