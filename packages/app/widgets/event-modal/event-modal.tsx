a5adff3dd5959c4732f7d75c140f80721310b8fe
mavix21 <marcevizcarra2109@hotmail.com>
2025-08-20 00:02:48 -0500

    refactor(EventModal): add disableDrag property to enhance user experience during modal interactions

--- packages/app/widgets/event-modal/event-modal.tsx
+++ packages/app/widgets/event-modal/event-modal.tsx
@@ -158,6 +158,7 @@ Check it out 👇`,
       <Sheet
         dismissOnSnapToBottom
         forceRemoveScrollEnabled={toggleEvent}
+        disableDrag
         modal
         open={toggleEvent}
         onOpenChange={setToggleEvent}
